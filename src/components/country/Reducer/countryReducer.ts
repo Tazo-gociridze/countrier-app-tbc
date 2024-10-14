a94f6fb4f63df183c167b7b46b039b9653f7305b
Tamaz Gociridze <117089051+Tazo-gociridze@users.noreply.github.com>
2024-10-14 22:49:42 +0400

    resolve type error

--- src/components/country/Reducer/countryReducer.ts
+++ src/components/country/Reducer/countryReducer.ts
@@ -13,84 +13,101 @@ export interface CountryAction {
     | "ADD_COUNTRY"
     | "DELETE_COUNTRY"
     | "REVIVE_COUNTRY";
-  payload?: { index: number; countries?: CountryData[] } | CountryData;
+  payload?: { index: number | string; countries?: CountryData[] } | CountryData;
 }
 
 export const countryReducer = (
   state: CountryState,
   action: CountryAction
 ): CountryState => {
-
   switch (action.type) {
-
-    case 'SORT_UP':
+    case "SORT_UP":
       return {
         ...state,
         countries: [
-          ...state.countries.filter((country) => !country.isDeleted).sort((a, b) => (b.likes || 0) - (a.likes || 0)),
-          ...state.countries.filter((country) => country.isDeleted)
-        ]
+          ...state.countries
+            .filter((country) => !country.isDeleted)
+            .sort((a, b) => (b.likes || 0) - (a.likes || 0)),
+          ...state.countries.filter((country) => country.isDeleted),
+        ],
       };
 
-    case 'SORT_DOWN':
+    case "SORT_DOWN":
       return {
         ...state,
         countries: [
-          ...state.countries.filter((country) => !country.isDeleted).sort((a, b) => (a.likes || 0) - (b.likes || 0)),
-          ...state.countries.filter((country) => country.isDeleted)
-        ]
+          ...state.countries
+            .filter((country) => !country.isDeleted)
+            .sort((a, b) => (a.likes || 0) - (b.likes || 0)),
+          ...state.countries.filter((country) => country.isDeleted),
+        ],
       };
 
-    case 'UPDATE_COUNTRIES':
-      return { ...state, countries: action.payload?.countries || state.countries };
+    case "UPDATE_COUNTRIES":
+      return {
+        ...state,
+        countries: action.payload?.countries || state.countries,
+      };
 
-    case 'INCREMENT_LIKE':
+    case "INCREMENT_LIKE":
       return {
         ...state,
         countries: state.countries.map((country, index) =>
-          index === action.payload?.index ? { ...country, likes: country.likes + 1 } : country
-        )
+          index === action.payload?.index
+            ? { ...country, likes: country.likes + 1 }
+            : country
+        ),
       };
-
 
     case "ADD_COUNTRY":
       return {
         ...state,
-        countries: [action.payload as CountryData, ...state.countries], 
+        countries: [action.payload as CountryData, ...state.countries],
       };
-
 
     case "DELETE_COUNTRY":
-      const deletedCountries = [...state.countries];
-        //@ts-ignore
-      deletedCountries[action.payload?.index].isDeleted = true;
+      if (
+        typeof action.payload?.index === "number" &&
+        action.payload.index >= 0 &&
+        action.payload.index < state.countries.length
+      ) {
+        const deletedCountries = [...state.countries];
+        const deletedCountry = { ...deletedCountries[action.payload.index] };
+        deletedCountry.isDeleted = true;
+        deletedCountries.splice(action.payload.index, 1);
+        deletedCountries.push(deletedCountry);
+        return {
+          ...state,
+          countries: deletedCountries,
+        };
+      } else {
+        console.error(
+          "action.payload.index is undefine"
+        );
+        return state;
+      }
 
-      const deletedCountry = deletedCountries.splice(
-        //@ts-ignore
-        action.payload?.index,
-        1
-      )[0];
-      deletedCountries.push(deletedCountry);
-
-      return {
-        ...state,
-        countries: deletedCountries,
-      };
-
-
-
-      case 'REVIVE_COUNTRY':
+    case "REVIVE_COUNTRY":
+      if (
+        typeof action.payload?.index === "number" &&
+        action.payload.index >= 0 &&
+        action.payload.index < state.countries.length
+      ) {
         const revivedCountries = [...state.countries];
-        //@ts-ignorets-ignore
-        const revivedCountry = revivedCountries.splice(action.payload?.index, 1)[0];
+        const revivedCountry = { ...revivedCountries[action.payload.index] };
+
         revivedCountry.isDeleted = false;
+        revivedCountries.splice(action.payload.index, 1);
         revivedCountries.unshift(revivedCountry);
-  
+
         return {
           ...state,
           countries: revivedCountries,
         };
-
+      } else {
+        console.error("action.payload.index is undefined");
+        return state;
+      }
 
     default:
       return state;
