6c44aaa5f161b280b9b0968be806406ceaa1dc2e
Tamaz Gociridze <117089051+Tazo-gociridze@users.noreply.github.com>
2024-10-16 21:55:39 +0400

    added for <CountryAddForm/> inputs limit of max symbols

--- src/components/country/country-components/countrySection/CountryAddForm.tsx
+++ src/components/country/country-components/countrySection/CountryAddForm.tsx
@@ -2,55 +2,89 @@ import { CountryAction } from "@components/country/Reducer/countryReducer";
 import { CountryData } from "@components/country/static/Interfaces";
 import { Dispatch, FC, useState } from "react";
 
-export interface DispatchType{
-    dispatch: Dispatch<CountryAction>
+export interface DispatchType {
+  dispatch: Dispatch<CountryAction>;
 }
 
-const CountryAddForm: FC<DispatchType> = ({dispatch}) => {
-
+const CountryAddForm: FC<DispatchType> = ({ dispatch }) => {
   const [newCountryName, setNewCountryName] = useState("");
   const [newCountryCapital, setNewCountryCapital] = useState("");
   const [newCountryPopulation, setNewCountryPopulation] = useState("");
 
-    const handleAddCountry = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        const newCountry: CountryData = {
-          id: String(Math.random()),
-          flagName: "newCountry",
-          name: newCountryName,
-          capital: newCountryCapital,
-          population: newCountryPopulation || "0",
-          likes: 0,
-        };
-        dispatch({ type: "ADD_COUNTRY", payload: newCountry });
-        setNewCountryName("");
-        setNewCountryCapital("");
-        setNewCountryPopulation("");
-      };
+  const [nameError, setNameError] = useState("");
+  const [capitalError, setCapitalError] = useState("");
+  const [populationError, setPopulationError] = useState("");
+  const handleAddCountry = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newCountry: CountryData = {
+      id: String(Math.random()),
+      flagName: "newCountry",
+      name: newCountryName,
+      capital: newCountryCapital,
+      population: newCountryPopulation || "0",
+      likes: 0,
+    };
+
+    if (
+      !nameError &&
+      newCountryName.length !== 0 &&
+      !capitalError &&
+      newCountryCapital.length !== 0 &&
+      !populationError &&
+      newCountryPopulation.length !== 0
+    ) {
+      dispatch({ type: "ADD_COUNTRY", payload: newCountry });
+      setNewCountryName("");
+      setNewCountryCapital("");
+      setNewCountryPopulation("");
+    }
+  };
+
+  const countryNameTargetHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewCountryName(e.target.value);
+    setNameError(e.target.value.length > 8 ? "the most length name" : "");
+  };
+
+  const countryCapitalTargetHandler = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setNewCountryCapital(e.target.value);
+    setCapitalError(e.target.value.length > 8 ? "the most length name" : "");
+  };
+
+  const countryPopulationTargetHandler = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setNewCountryPopulation(e.target.value);
+    setPopulationError(e.target.value.length > 8 ? "the most length name" : "");
+  };
 
   return (
-    <form onSubmit={handleAddCountry} action="">
-            <input
-              type="text"
-              value={newCountryName}
-              onChange={(e) => setNewCountryName(e.target.value)}
-              placeholder="country name"
-            />
-            <input
-              type="text"
-              value={newCountryCapital}
-              onChange={(e) => setNewCountryCapital(e.target.value)}
-              placeholder="capital"
-            />
-            <input
-              type="number"
-              value={newCountryPopulation}
-              onChange={(e) => setNewCountryPopulation(e.target.value)}
-              placeholder="population"
-            />
-            <button>add country</button>
+    <form className="create-country-form" onSubmit={handleAddCountry} action="">
+      {nameError && <p>max limit of symbols - 8</p>}
+      <input
+        type="text"
+        value={newCountryName}
+        onChange={countryNameTargetHandler}
+        placeholder="country name"
+      />
+      {capitalError && <p>min limit of symbols - 8</p>}
+      <input
+        type="text"
+        value={newCountryCapital}
+        onChange={countryCapitalTargetHandler}
+        placeholder="capital"
+      />
+      {populationError && <p>max limit of symbols - 8</p>}
+      <input
+        type="number"
+        value={newCountryPopulation}
+        onChange={countryPopulationTargetHandler}
+        placeholder="population"
+      />
+      <button>add country</button>
     </form>
-  )
+  );
 };
 
-export default CountryAddForm; 
+export default CountryAddForm;
